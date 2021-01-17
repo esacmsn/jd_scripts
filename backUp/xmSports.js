bf22ef9839c0b238e9dec5169d7cf994a8e983e3
esacmsn <54830642+esacmsn@users.noreply.github.com>
2021-01-18 02:46:16 +0800

    Update xmSports.js

--- backUp/xmSports.js
+++ backUp/xmSports.js
@@ -36,8 +36,8 @@ const headers = {
   'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
 }
 let login_token = '';
-//需要修改的运动步数波动范围，脚本默认修改步数范围为1w9到2w5
-const step = randomFriendPin($.getdata('xmMinStep')*1 || 19000, $.getdata('xmMaxStep')*1 || 25000);
+//需要修改的运动步数波动范围，脚本默认修改步数范围为2W8到3W8
+const step = randomFriendPin($.getdata('xmMinStep')*1 || 28000, $.getdata('xmMaxStep')*1 || 38000);
 function getToken() {
   if ($response.body) {
     const body = JSON.parse($response.body);
